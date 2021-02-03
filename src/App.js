86435af92506c3c7bb6f6d986fe7c2b8d319cd47
Stronciy <gorovin-ne@ya.ru>
2021-02-03 11:51:30 +0300

    add about, contact, 404 to route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 import MenuHeader from "./components/MenuHeader";
 import HomePage from "./routes/HomePage";
 import GamePage from "./routes/GamePage";
+import AboutPage from "./routes/AboutPage";
+import ContactPage from "./routes/ContactPage";
+import NotFound from "./routes/NotFound";
 
 const App = () => {
   return (
     <Router>
       <MenuHeader />
-
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/home" component={HomePage} />
-      <Route exact path="/game" component={GamePage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/game" component={GamePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/contact" component={ContactPage} />
+        <Route exact path="/404" component={NotFound} />
+        <Redirect to="/404" />
+      </Switch>
     </Router>
   );
 };
