541726f3fb6b6f5b4614ddfaebeb24350b914846
Stronciy <gorovin-ne@ya.ru>
2021-02-03 16:57:54 +0300

    styling, add cards to GamePage

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,8 @@ const Header = ({ title, description }) => {
   return (
     <header className={s.root}>
       <div className={s.forest}></div>
+      <div className={s.silhouette}></div>
+      <div className={s.moon}></div>
       <div className={s.container}>
         <h1>{title}</h1>
         <p>{description}</p>
--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -2,11 +2,26 @@ import { NavLink } from "react-router-dom";
 
 import s from "./GamePage.module.css";
 
+import { POKEMONS } from "./../../assets/POKEMONS";
+import PokemonCard from "./../../components/PokemonCard";
+
 const GamePage = () => {
   return (
     <div className={s.plug}>
       <h1>This is Game</h1>
       <p>Battles will begin soon.</p>
+      <>
+        {POKEMONS.map((item) => (
+          <PokemonCard
+            name={item.name}
+            img={item.img}
+            type={item.type}
+            values={item.values}
+            id={item.id}
+            key={item.id}
+          />
+        ))}
+      </>
       <NavLink exact to="/" activeClassName="active" className={s["nav-link"]}>
         Back to Home
       </NavLink>
