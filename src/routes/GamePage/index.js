e5d11fb228da562e743a85457ea8f1a66afe19a0
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-02-17 03:50:42 +0300

    revorked pokemos to work with firebase

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,40 +1,79 @@
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import database from "../../services/firebase";
+
+// import LayoutBg from '../../assets/bg1.jpg';
 
 import s from "./GamePage.module.css";
 
-import { POKEMONS } from "./../../assets/POKEMONS";
 import PokemonCard from "./../../components/PokemonCard";
 
 const GamePage = () => {
-  const [pokemonsArr, SetPokemonsArr] = useState(POKEMONS);
+  const [pokemons, SetPokemons] = useState({});
 
   const onCard = (pokemonId) => {
-    SetPokemonsArr((state) =>
-      state.map((item) =>
-        item.id === pokemonId ? { ...item, isActive: !item.isActive } : item
-      )
-    );
+    SetPokemons((prevState) => {
+      return Object.entries(prevState).reduce((acc, item) => {
+        const pokemon = { ...item[1] };
+        if (pokemon.id === pokemonId) {
+          pokemon.isActive = !pokemon.isActive;
+          database.ref("pokemons/" + item[0]).set({
+            ...pokemon,
+          });
+        }
+        acc[item[0]] = pokemon;
+        return acc;
+      }, {});
+    });
   };
+
+  const handleAddPokeClick = () => {
+    const newPostKey = database.ref().child("pokemons").push().key;
+    const newPoke = Object.entries(pokemons)[0][1];
+    //TODO: remove
+    //TEMP id gen
+    newPoke.id =
+      Object.entries(pokemons).reduce(
+        (max, item) => (item[1].id > max ? item[1].id : max),
+        Object.entries(pokemons)[0][1].id
+      ) + 1;
+    newPoke.active = false;
+    //TEMP
+    database.ref("pokemons/" + newPostKey).set(newPoke);
+
+    getAll();
+  };
+
+  const getAll = () => {
+    database.ref("pokemons").once("value", (snapshot) => {
+      SetPokemons(snapshot.val());
+    });
+  };
+
+  useEffect(() => {
+    getAll();
+  }, []);
 
   return (
     <div className={s.plug}>
       <h1>This is Game</h1>
       <p>Battles will begin soon.</p>
+      <button onClick={handleAddPokeClick}>Add pokemon</button>
       <div className={s.flex}>
-        {pokemonsArr &&
-          pokemonsArr.map((item) => (
+        {Object.entries(pokemons).map(
+          ([key, { id, type, img, name, values, bgImg, isActive }]) => (
             <PokemonCard
-              name={item.name}
-              img={item.img}
-              type={item.type}
-              values={item.values}
-              id={item.id}
-              key={item.id}
+              name={name}
+              img={img}
+              type={type}
+              values={values}
+              id={id}
+              key={key}
               onClick={onCard}
-              isActive={item.isActive}
+              isActive={isActive}
             />
-          ))}
+          )
+        )}
       </div>
       <NavLink exact to="/" activeClassName="active" className={s["nav-link"]}>
         Back to Home
