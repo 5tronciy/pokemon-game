15fe2ce2b2eed07776e59818fd592ca661166b17
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-02-18 21:02:08 +0300

    add Context

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,4 +1,7 @@
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Route, Switch, useRouteMatch, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import database from "../../services/firebase";
+import { PokemonContext } from "../../context/pokemonContext";
 
 import StartPage from "./Start";
 import BoardPage from "./Board";
@@ -6,12 +9,74 @@ import FinishPage from "./Finish";
 
 const GamePage = () => {
   const match = useRouteMatch();
+
+  const [pokemons, SetPokemons] = useState({});
+
+  const [pokemonsSelected, SetPokemonsSelected] = useState([]);
+
+  const history = useHistory();
+
+  const onGameStart = () => {
+    history.push("/game/board");
+  };
+
+  const onCard = (pokemonId) => {
+    Object.entries(pokemons).reduce((acc, item) => {
+      if (item[1].id === pokemonId) {
+        const pokemon = { ...item[1] };
+        if (
+          pokemonsSelected.length < 5 &&
+          !pokemonsSelected.some((i) => i.id === pokemonId)
+        ) {
+          pokemon.isSelected = !pokemon.isSelected;
+          SetPokemons((prevState) => {
+            return { ...prevState, [item[0]]: pokemon };
+          });
+          SetPokemonsSelected((prevState) => {
+            return [...prevState, { ...pokemon }];
+          });
+        } else if (
+          pokemonsSelected.length > 0 &&
+          pokemonsSelected.some((i) => i.id === pokemonId)
+        ) {
+          pokemon.isSelected = !pokemon.isSelected;
+          SetPokemons((prevState) => {
+            return { ...prevState, [item[0]]: pokemon };
+          });
+          SetPokemonsSelected((prevState) =>
+            prevState.filter((i) => i.id !== pokemonId)
+          );
+        }
+      }
+      return null;
+    }, {});
+  };
+
+  const syncStateWithDatabase = () => {
+    database.ref("pokemons").once("value", (snapshot) => {
+      SetPokemons(snapshot.val());
+    });
+  };
+
+  useEffect(() => {
+    syncStateWithDatabase();
+  }, []);
+
   return (
-    <Switch>
-      <Route path={`${match.path}/`} exact component={StartPage} />
-      <Route path={`${match.path}/board`} component={BoardPage} />
-      <Route path={`${match.path}/finish`} component={FinishPage} />
-    </Switch>
+    <PokemonContext.Provider
+      value={{
+        onCard,
+        onGameStart,
+        pokemons,
+        pokemonsSelected,
+      }}
+    >
+      <Switch>
+        <Route path={`${match.path}/`} exact component={StartPage} />
+        <Route path={`${match.path}/board`} component={BoardPage} />
+        <Route path={`${match.path}/finish`} component={FinishPage} />
+      </Switch>
+    </PokemonContext.Provider>
   );
 };
 
