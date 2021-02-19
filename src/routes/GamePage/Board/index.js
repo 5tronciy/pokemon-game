c67a3f63750621653336f587f8b988ee012537ac
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-02-19 14:59:27 +0300

    add 2nd player and card hover anim

--- src/routes/GamePage/Board/index.js
+++ src/routes/GamePage/Board/index.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import PokemonCard from "../../../components/PokemonCard";
 import { PokemonContext } from "../../../context/pokemonContext";
@@ -6,12 +6,39 @@ import { PokemonContext } from "../../../context/pokemonContext";
 import s from "./style.module.css";
 
 const BoardPage = () => {
-  const { pokemonsSelected } = useContext(PokemonContext);
   const history = useHistory();
-
+  const [board, SetBoard] = useState([]);
+  const [oponent, setOponent] = useState([]);
+  const { pokemonsSelected } = useContext(PokemonContext);
   if (Object.keys(pokemonsSelected).length < 1) {
     history.replace("/game");
   }
+  const syncBoard = async () => {
+    const boardResp = await fetch(
+      "https://reactmarathon-api.netlify.app/api/board"
+    );
+    const boardData = await boardResp.json();
+    SetBoard(boardData.data);
+  };
+
+  const initOponent = async () => {
+    const oponentResp = await fetch(
+      "https://reactmarathon-api.netlify.app/api/create-player"
+    );
+    const oponentData = await oponentResp.json();
+    setOponent(oponentData.data);
+  };
+
+  useEffect(() => {
+    syncBoard();
+
+    initOponent();
+  }, []);
+
+  const onBoardPlate = (position) => {
+    console.log(position);
+  };
+
   return (
     <div className={s.root}>
       <div className={s.playerOne}>
@@ -36,15 +63,37 @@ const BoardPage = () => {
           ))}
       </div>
       <div className={s.board}>
-        <div className={s.boardPlate}>1</div>
-        <div className={s.boardPlate}>2</div>
-        <div className={s.boardPlate}>3</div>
-        <div className={s.boardPlate}>4</div>
-        <div className={s.boardPlate}>5</div>
-        <div className={s.boardPlate}>6</div>
-        <div className={s.boardPlate}>7</div>
-        <div className={s.boardPlate}>8</div>
-        <div className={s.boardPlate}>9</div>
+        {board &&
+          board.map((item) => {
+            return (
+              <div
+                className={s.boardPlate}
+                key={item.position}
+                onClick={() => !item.card && onBoardPlate(item.position)}
+              >
+                {item.card && <PokemonCard {...item} minimize />}
+              </div>
+            );
+          })}
+      </div>
+      <div className={s.playerTwo}>
+        {oponent &&
+          oponent.map(({ id, type, img, name, values, bgImg }, index) => (
+            <PokemonCard
+              key={`${id}-${index}`}
+              id={id}
+              type={type}
+              img={img}
+              name={name}
+              values={values}
+              bgImg={bgImg}
+              isActive={true}
+              minimize={true}
+              isSelected={false}
+              className={s.card}
+              onClick={() => console.log("clicked")}
+            />
+          ))}
       </div>
     </div>
   );
